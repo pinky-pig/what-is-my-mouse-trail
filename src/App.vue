a452cbe6f4267bdcacc19440c827e692f589cc16
pinky-pig <2545589993@qq.com>
2023-07-03 14:12:24 +0800

    main styles

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 import MouseTrail from './components/MouseTrail.vue';
 import Draw from './components/Draw.vue';
 import ColorPicker from './components/ColorPicker.vue';
@@ -9,81 +10,62 @@ import { currentMode } from './components/utils';
 
 <template>
   <div class="playground">
-    <div class="content">
-      <MouseTrail />
-      <Draw />
+    <MouseTrail />
+    <Draw />
 
-      <p class="place-holder-text">{{ currentMode }}</p>
-    </div>
-    <footer>
-      <div class="footer-left">
-        <ModePicker></ModePicker>
-        <ColorPicker></ColorPicker>
-      </div>
-
-      <a href="https://github.com/pinky-pig/what-is-my-mouse-trail/tree/refactor" class="footer-right" target="_blank">
-        GitHub
-      </a>
-    </footer>
+    <p class="place-holder-text">{{ currentMode }}</p>
   </div>
+
+  <footer class="footer">
+    <div class="left">
+      <ModePicker />
+      <ColorPicker />
+    </div>
+
+    <a  href="https://github.com/pinky-pig/what-is-my-mouse-trail/tree/refactor" class="right" target="_blank">
+      GitHub
+    </a>
+  </footer>
 </template>
+
 <style scoped>
-.playground {
-  width: 100%;
-  height: 100%;
-  position: absolute;
+.playground{
+  width: 80vw;
+  height: 80vh;
+  outline: 10px solid #474747;
+  background: #f7f4f0;
+  border-radius: 10px;
+  border-width: 1px;
+  position: relative;
   overflow: hidden;
-  background: #F3EDE1;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  padding: 5% 10% 15%;
-  box-sizing: border-box;
 }
-
-.content {
+.footer{
   width: 100%;
-  height: 100%;
-  border-radius: 20px;
-  border: 5px solid #000000;
-  text-align: center;
-}
-
-.place-holder-text {
-  color: #000000;
-  font-size: 18px;
-  font-style: italic;
-  font-family: fantasy;
-  position: absolute;
-  top: 0;
-}
-
-footer {
-  width: 100%;
-  height: 80px;
-  margin-top: 20px;
+  height: 50px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  margin-top: 15px;
 }
-
-.footer-left {
+.left{
   display: flex;
   flex-direction: row;
-  justify-content: start;
-  align-items: center;
-  gap: 40px;
+  gap: 20px;
 }
-
-.footer-right {
-  color: #000000;
-  cursor: pointer;
-  font-size: 24px;
+.right{
+  margin: 0 10px;
+  color: #474747;
+  text-decoration: none;
+  margin: 10px 10px;
   font-weight: 800;
+  font-size: 20px;
+  cursor: pointer;
+  padding: 10px;
 }
-.footer-right:hover {
-  color: #b8b8b8;
+.right:hover{
+  color: #f7f4f0;
+  background: #474747;
+  border-radius: 5px;
 }
 </style>
